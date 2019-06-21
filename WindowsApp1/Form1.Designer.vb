33a5bb5c78828d1817860787c7d53130f769d4a3
Karen Payne <paynekaren@comcast.net>
2019-06-21 14:04:02 -0700

    Tweaks

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -23,29 +23,67 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(7, 17)
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(360, 212)
+        Me.DataGridView1.Size = New System.Drawing.Size(718, 403)
         Me.DataGridView1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 339)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(718, 64)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(19, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(100, 17)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(379, 241)
+        Me.ClientSize = New System.Drawing.Size(718, 403)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,33 +1,10 @@
-﻿Public Class Form1
-    Private _comboColumnName As String = "C1"
+﻿
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim source = {"Add", "Subtract", "Divide", "Multiply"}
+    End Sub
 
-        Dim dataGridViewCellStyle1 = New DataGridViewCellStyle With
-                {
-                    .Alignment = DataGridViewContentAlignment.MiddleLeft
-                }
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
-        Dim column1 = New DataGridViewComboBoxColumn With
-                {
-                    .DataSource = source,
-                    .DisplayStyle = DataGridViewComboBoxDisplayStyle.Nothing,
-                    .DefaultCellStyle = dataGridViewCellStyle1,
-                    .Name = _comboColumnName, .HeaderText = "Demo",
-                    .SortMode = DataGridViewColumnSortMode.NotSortable
-                }
-
-        Dim column2 = New DataGridViewCheckBoxColumn With {.Name = "C2", .HeaderText = "Col 2"}
-
-        Dim column3 = New DataGridViewTextBoxColumn With {.Name = "C3", .HeaderText = "Col 3"}
-
-        DataGridView1.Columns.AddRange(column1, column2, column3)
-
-        DataGridView1.Rows.Add("Divide", True, "Karen")
-        DataGridView1.Rows.Add("Add", False, "Kevin")
-        DataGridView1.Rows.Add("Subtract", True, "Anne")
-        DataGridView1.Rows.Add("Add", True, "Joe")
-        DataGridView1.Rows.Add("Multiply", True, "Jean")
     End Sub
 End Class
